50096772f24d3a6d993d7de02d1b8124a3bd98c3
Matheus Mayor <105829928+MatheusMayor@users.noreply.github.com>
2022-09-16 01:45:22 -0300

    Part1

---
+++ main.js
@@ -0,0 +1,12 @@
+function tocasomPom () {
+    document.querySelector('#som_tecla_pom]').play();
+
+}
+
+function tocasomClap () {
+    document.querySelector('#som_tecla_clap]').play();
+
+}
+
+document.querySelector('tecla-pom');
+
